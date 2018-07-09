b01892a8e1cfdb0a58346fdddd30dc102e750d96
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-09 19:14:59 -0400

    Initial commit

---
+++ src/main/java/com/darshan/firstprogram/FirstprogramApplication.java
@@ -0,0 +1,12 @@
+package com.darshan.firstprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprogramApplication.class, args);
+	}
+}
---
+++ src/main/java/com/darshan/firstprogram/HomeController.java
@@ -0,0 +1,40 @@
+package com.darshan.firstprogram;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+
+public class HomeController {
+
+    @RequestMapping("/")
+
+    public String homePage() {
+        return "index";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
